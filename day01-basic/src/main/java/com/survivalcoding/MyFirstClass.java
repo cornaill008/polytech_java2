0df271a848844757b1ac7945fa60851f11a06ecd
Inho.Seo <cornaill008@gmail.com>
2026-06-08 10:33:02 +0900

    String 연습

--- day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
+++ day01-basic/src/main/java/com/survivalcoding/MyFirstClass.java
@@ -1,4 +1,17 @@
 package com.survivalcoding;
 
+import java.sql.SQLOutput;
+
 public class MyFirstClass {
+    // main
+    public static void main(String[] args) {
+        int i = 0;
+        i = 10;
+
+        String name = "홍길동";
+
+        System.out.println("Hello"); //sout
+
+
+    }
 }
