e2cbc4298ee2c7f57e5e90f3aef10693a0f6af4a
Inho.Seo <cornaill008@gmail.com>
2026-06-09 10:55:27 +0900

    Add Cleric implementation and test

--- day02-instance-class/src/main/java/com/survivalcoding/Cleric.java
+++ day02-instance-class/src/main/java/com/survivalcoding/Cleric.java
@@ -5,22 +5,27 @@ import java.util.Random;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Cleric {
+    final int maxHp = 50;
+    final int maxMp = 10;
+    final int selfAidMpCost = 5;
+
     String name;
-    int hp;
-    int mp;
+    int hp = maxHp;
+    int mp = maxMp;
 
     // MP를 5 소비하고 HP를 최대 50까지 회복한다.
     void selfAid() {
-        if (mp - 5 > 0) {
-            mp -= 5;
-            hp = Math.min(hp + 50, 50);
+        if (mp < selfAidMpCost) {
+            return;
         }
+        mp -= selfAidMpCost;
+        hp = maxHp;
     }
 
     // 기도한 시간에 0~2의 랜덤 보정을 더해 MP를 회복한다.
-    int pray(int seconds) {
+    int pray(int sec) {
         int beforeMp = mp;
-        int rec = seconds + new Random().nextInt(3);
+        int rec = sec + new Random().nextInt(3);
 
         mp = Math.min(mp + rec, 10);
 
--- day02-instance-class/src/test/java/com/survivalcoding/ClericTest.java
+++ day02-instance-class/src/test/java/com/survivalcoding/ClericTest.java
@@ -3,11 +3,32 @@ package com.survivalcoding;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ClericTest {
 
     @Test
     @DisplayName("selfAid를 하면 HP를 최대로 회복한다")
     void selfAid() {
+        final int initialhp = 5;
+        final int initialmp = 10;
+
+        Cleric cleric = new Cleric();
+
+        cleric.hp = initialhp;
+        cleric.mp = initialmp;
+
+        cleric.selfAid();
+        assertEquals(cleric.maxHp, cleric.hp);
+
+        final int mpEdgeCase = 0;
+        cleric.hp = initialhp;
+        cleric.mp = mpEdgeCase;
+
+        cleric.selfAid();
+        assertEquals(cleric.maxHp, cleric.hp);
+
+
         // given (준비)
 
         // when (실행)
