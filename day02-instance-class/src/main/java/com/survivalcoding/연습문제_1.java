eaf02029b5633b99cf11fa09c97c55698885e447
Inho.Seo <cornaill008@gmail.com>
2026-06-16 15:40:06 +0900

    연습문제_1

---
+++ day02-instance-class/src/main/java/com/survivalcoding/연습문제_1.java
@@ -0,0 +1,28 @@
+package com.survivalcoding;
+
+public class 연습문제_1 {
+    /* 1. Person Student    (O)
+    2. car  Engine         (X)
+    3. Father Child        (X)
+    4. Food Sushi          (O)
+    5. SuperMan Man        (X)
+
+
+
+     */
+}
+
+
+/* 연습문제_2
+               1                  2               3
+부모 클래스 ElectronicDevice      Vehicle         Book
+
+
+              Phone               Car        Dictionary
+
+
+
+자식 클래스  I-Phone             Bus           EnglishDictionary
+
+
+ */
